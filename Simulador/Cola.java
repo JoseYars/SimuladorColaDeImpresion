a708f33d4ee2aa8b6ad02851abd418ea80629c10
JoseYars <133066131+JoseYars@users.noreply.github.com>
2023-09-28 17:55:58 -0600

    Add files via upload

---
+++ Simulador/Cola.java
@@ -0,0 +1,73 @@
+public class Cola {
+private Nodo frente;
+private Nodo atras;
+private int longitud;
+public Cola() {
+frente = null;
+atras = null;
+longitud = 0;
+}
+public void insertar(String dato) {
+Nodo nuevoNodo = new Nodo(dato);
+if (estaVacia()) {
+frente = nuevoNodo;
+atras = nuevoNodo;
+atras.setSiguiente(frente);
+} else {
+atras.setSiguiente(nuevoNodo);
+atras = nuevoNodo;
+atras.setSiguiente(frente);
+}
+longitud++;
+}
+
+public String quitar() {
+    if (frente == null) {
+        return null; // La cola está vacía
+    }
+    String archivo = frente.getDato();
+    frente = frente.getSiguiente();
+    if (frente == null) {
+        atras = null;
+    }
+    longitud--;
+    return archivo;
+}
+
+public boolean estaVacia() {
+return frente == null;
+}
+
+public Nodo getFrente() {
+    if (!estaVacia()) {
+        return frente;
+    } else {
+        System.out.println("La cola está vacía.");
+        return null;
+    }
+}
+
+public Nodo getAtras() {
+    if (!estaVacia()) {
+        return atras;
+    } else {
+        System.out.println("La cola está vacía.");
+        return null;
+    }
+}
+
+public int getLongitud() {
+    return longitud;
+}
+
+
+
+public void vaciar() {
+    frente = null;
+    atras = null;
+    longitud = 0;
+}
+
+
+
+}
---
+++ Simulador/Main.java
@@ -0,0 +1,58 @@
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Cuantas impresiones quiere realizar?");
+        int numImp = leer.nextInt();
+
+        // Consumir la nueva línea después de leer el número
+        leer.nextLine();
+
+        Cola colaC = new Cola();
+
+        for (int i = 1; i <= numImp; i++) {
+            System.out.println("NOMBRE Y DIRECCION DEL ARCHIVO " + i + ":");
+            String archivo = leer.nextLine();
+            colaC.insertar(archivo);
+        }
+
+        // Imprimir los archivos
+        System.out.println("\nArchivos a imprimir:");
+        int archivoNumero = 1;
+        while (archivoNumero <= numImp) {
+            String archivo = colaC.quitar();
+            if (archivo != null) {
+                System.out.println("Archivo " + archivoNumero + ": " + archivo);
+
+                // Abre el cuadro de diálogo de impresión del sistema para el archivo
+                abrirOpcionesDeImpresion(archivo);
+
+                archivoNumero++;
+            } else {
+                System.out.println("La cola está vacía.");
+                break;
+            }
+        }
+
+        leer.close();
+    }
+
+    // Método para abrir el cuadro de diálogo de opciones de impresión del sistema para un archivo
+    private static void abrirOpcionesDeImpresion(String filePath) {
+        File archivo = new File(filePath);
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.PRINT)) {
+            try {
+                Desktop.getDesktop().print(archivo);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("La impresión no es compatible en este sistema.");
+        }
+    }
+}
