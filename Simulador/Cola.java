cf2f7711161def443ddf2f0d9d7c292ec97b42be
carlA <74275311+carlasalazar@users.noreply.github.com>
2023-09-28 21:06:06 -0600

    Update Cola.java

--- Simulador/Cola.java
+++ Simulador/Cola.java
@@ -1,24 +1,26 @@
 public class Cola {
-private Nodo frente;
-private Nodo atras;
-private int longitud;
+    private Nodo frente;
+    private Nodo atras;
+    private int longitud;
+
 public Cola() {
-frente = null;
-atras = null;
-longitud = 0;
-}
+    frente = null;
+    atras = null;
+    longitud = 0;
+    }
+    
 public void insertar(String dato) {
-Nodo nuevoNodo = new Nodo(dato);
-if (estaVacia()) {
-frente = nuevoNodo;
-atras = nuevoNodo;
-atras.setSiguiente(frente);
-} else {
-atras.setSiguiente(nuevoNodo);
-atras = nuevoNodo;
-atras.setSiguiente(frente);
-}
-longitud++;
+    Nodo nuevoNodo = new Nodo(dato);
+    if (estaVacia()) {
+        frente = nuevoNodo;
+        atras = nuevoNodo;
+        atras.setSiguiente(frente);
+    } else {
+        atras.setSiguiente(nuevoNodo);
+        atras = nuevoNodo;
+        atras.setSiguiente(frente);
+        }
+        longitud++;
 }
 
 public String quitar() {
@@ -27,15 +29,15 @@ public String quitar() {
     }
     String archivo = frente.getDato();
     frente = frente.getSiguiente();
-    if (frente == null) {
-        atras = null;
-    }
+        if (frente == null) {
+            atras = null;
+        }
     longitud--;
     return archivo;
 }
 
 public boolean estaVacia() {
-return frente == null;
+    return frente == null;
 }
 
 public Nodo getFrente() {
@@ -60,14 +62,10 @@ public int getLongitud() {
     return longitud;
 }
 
-
-
 public void vaciar() {
     frente = null;
     atras = null;
     longitud = 0;
 }
-
-
 
 }
